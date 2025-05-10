a15e4c0a1a8630b3031896cfcc77f55133688dc2
VIKASHKUMAWAT9024 <vikashkumawat3079@gmail.comgit config --listgit config --global user.email vikashkumawat3079@gmail.com>
2025-05-10 11:05:20 +1100

    Viakssh kumwwat

--- vite-project/src/component/Navbar.jsx
+++ vite-project/src/component/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
         <div className='container py-2 flex justify-center md:justify-between justify-between items-center'>
               <div className='text-2xl font-bold hidden md:inline'>Vikash Kumawat</div>
               <div className='space-x-6'>
-                <a href="#home" className='hover:text-gray-400'>Home</a>
+                <a href="#home" className='hover:text-gray-400 ' >Home</a>
                 <a href="#about" className='hover:text-gray-400'>About Me</a>
                 <a href="#services" className='hover:text-gray-400'>Services</a> 
                 <a href="#projects" className='hover:text-gray-400'>Projects</a>
